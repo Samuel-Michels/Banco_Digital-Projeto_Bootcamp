d8fde21fac021fde9c563cc8ed9f6efa1dced125
Samuel_Michels <77849693+Samuel-Michels@users.noreply.github.com>
2022-10-10 21:57:22 -0300

    Adicionados Features: -Método de Saque -Método de Deposito -Método de Transferencia -Consulta Extrato

--- src/Main.java
+++ src/Main.java
@@ -21,11 +21,16 @@ public class Main {
         Boolean isContaEntrarValida = false;
         String decisao;
 
-        double saldoTotal = 0, saldoContaCorrente = 0, saldoChequeEspecial = 0, saldoPoupança = 0;
+        double saldoTotal = 0,
+                saldoContaCorrente = 0,
+                saldoChequeEspecial = 0,
+                saldoInicialChequeEspecial = 0,
+                saldoPoupança = 0;
 
         Scanner scan = new Scanner(System.in);
         ContaDados dadosConta;
         IOJson ioJson = new IOJson();
+        MovimentoConta movimentoConta = new MovimentoConta();
 
         System.out.println("BEM VINDO AO BANCO DIGITAL TE AMO \n 1.Entrar \n 2.cadastrar\n->");
         decisao = scan.nextLine();
@@ -63,12 +68,12 @@ public class Main {
             //looping dados conta
             while(isContaEntrarValida){
 
-                if (decisao != "0"){
-                    saldoTotal = calcularSaldoTotalMain(saldoContaCorrente,saldoChequeEspecial);
-                }
+                decisao = "0";
+
+                saldoTotal = saldoContaCorrente + saldoChequeEspecial;
 
 
-                System.out.println("Saldo:R$"+saldoTotal+"\n1.Extrato Detalhado\n2.Sacar\n3.Depositar\n4.transferir");
+                System.out.println("Saldo:R$"+saldoContaCorrente+"\n1.Extrato Detalhado\n2.Sacar\n3.Depositar\n4.transferir");
                 if(dadosConta.getPoupanca()){
                     System.out.println("5.Depositar Poupança\n6.Retirar Poupança");
                 }
@@ -77,17 +82,31 @@ public class Main {
 
                 switch (decisao){
                     case "1":
-                        // TODO: 10/10/2022 fazer interface de extrato
+                        // 1.Extrato Detalhado
+                        movimentoConta.mostrarExtratoConta(dadosConta.getNome(),saldoContaCorrente,saldoChequeEspecial);
+                        decisao = scan.nextLine();
                         break;
 
                     case "2":
                         // TODO: 10/10/2022 fazer classe e metodo de saque
+                        System.out.println("Qual valor do Saque?\n Valor máximo disponível para saque R$"+saldoTotal);
+                        Double valorSaque = scan.nextDouble();
+
+                        saldoContaCorrente = movimentoConta.sacarDinheiro(valorSaque,saldoContaCorrente,saldoChequeEspecial);
                         break;
                     case "3":
                         // TODO: 10/10/2022 fazer classe e metodo de deposito
+                        System.out.println("Qual valor do Depósito?\n ->");
+                        Double valorDeposito = scan.nextDouble();
+
+                        saldoContaCorrente = movimentoConta.depositarDinheiro(valorDeposito,saldoContaCorrente);
                         break;
                     case "4":
                         // TODO: 10/10/2022 fazer classe e metodo de transferencia
+                        System.out.println("Qual valor do Saque?\n Valor máximo disponível para saque R$"+saldoTotal);
+                        Double valorTransferencia = scan.nextDouble();
+
+                        saldoContaCorrente = movimentoConta.transferirDinheiro(valorTransferencia,saldoContaCorrente,saldoChequeEspecial);
                         break;
                     case "5":
                         // TODO: 10/10/2022 fazer classe e metodo de Deposito poupança
@@ -120,16 +139,5 @@ public class Main {
 
         return valorDoChequeEspecial;
     }
-
-    public static Double calcularSaldoTotalMain(Double saldoContaCorrente, Double saldoChequeEspecial){
-        Double saldoTotal = saldoContaCorrente + saldoChequeEspecial;
-
-        return saldoTotal;
-    }
-
-
-
-
-
 
 }
--- src/MovimentoConta.java
+++ src/MovimentoConta.java
@@ -1,23 +1,84 @@
-public class MovimentoConta {/*
+import java.util.Scanner;
 
-    public Double sacarValor(Double valor){
+public class MovimentoConta {
 
+    DataCustom data = new DataCustom();
 
-        return saldoFinal;
+    public void mostrarExtratoConta(String nome,Double contaCorrente, Double chequeEspecial){
+        double saldoTotal = chequeEspecial + contaCorrente;
+
+        System.out.println("Olá, " + nome);
+        System.out.println("Extrato do dia: " + data.mostrarDataEHora());
+        System.out.println("---------------------");
+        System.out.println("Conta Corrente: R$"+contaCorrente);
+        System.out.println("Cheque Especial: R$"+chequeEspecial);
+        System.out.println("---------------------");
+        System.out.println("Saldo Total R$"+saldoTotal);
+        System.out.println("---------------------");
+        if (contaCorrente < 0){
+            System.out.println("Você tem cobranças pendentes, mantenha-se em dia com cheque especial\nEvite cobrança de Juros!");
+        }
+
+        System.out.println("*Fim");
+        System.out.println("Pressione Enter!");
     }
-    public Double transferirValor(Double valor){
 
+    public Double sacarDinheiro(Double valorSaque, Double saldoContaCorrente, Double saldoChequeEspecial){
+        Double saldoTotalDisponivelSaque = saldoContaCorrente+saldoChequeEspecial;
 
-        return saldoFinal;
+        if (valorSaque>saldoTotalDisponivelSaque){
+            System.out.println("Saldo insuficiente!");
+            return 0.0;
+        } else {
+            saldoContaCorrente -= valorSaque;
+            System.out.println("Saque efetuado com sucesso!");
+        }
+
+        return saldoContaCorrente;
     }
-    public Double depositarValor(Double valor){
+    public Double depositarDinheiro(Double valorDeposito, Double saldoContaCorrente){
+
+        if (valorDeposito < 0.0){
+            System.out.println("Valor inválido né meu querido?");
+        } else {
+            System.out.println("Procedido deposito de R$" + valorDeposito);
+
+            saldoContaCorrente += valorDeposito;
+        }
+
+        return saldoContaCorrente;
+    }
+    public Double transferirDinheiro(Double valorTranferencia, Double saldoContaCorrente, Double saldoChequeEspecial){
+        Double saldoTotalDisponivel = saldoContaCorrente+saldoChequeEspecial;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Conta: ");
+        String input1 = scan.nextLine();
+        System.out.println("Agencia: ");
+        String input2 = scan.nextLine();
+
+        if (!input1.equals("")){
+            if (valorTranferencia>saldoTotalDisponivel){
+                System.out.println("Saldo insuficiente!");
+                return 0.0;
+            } else {
+                saldoContaCorrente -= valorTranferencia;
+                System.out.println("Transação feita com sucesso efetuado com sucesso!");
+            }
+        } else{
+            System.out.println("Precisa preencher os dados!");
+        }
 
 
-        return saldoFinal;
+
+        return saldoContaCorrente;
     }
 
     public Boolean isMovimentoLegal(){
 
         return false;
-    }*/
+    }
+
+
 }
